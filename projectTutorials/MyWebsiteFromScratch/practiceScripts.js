4c9df2eb2ac1a67154ae06dbc392b1948fb36619
Osward Phekani <oswarddesigns@gmail.com>
2025-05-27 23:34:57 +0100

    Practice script

---
+++ projectTutorials/MyWebsiteFromScratch/practiceScripts.js
@@ -0,0 +1,14 @@
+const toDo = ["Code", "Gym", "Cook" ];
+   toDo.push("GoToSleep ");
+
+for(let i = 0; i < toDo.length; i++){
+// console.log(toDo[i]);
+}
+ 
+
+function halfNumbers(number){
+    
+const numberMultiply = number /2;
+console.log(numberMultiply);
+}
+halfNumbers(20);
