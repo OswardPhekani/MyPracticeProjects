55a4723f8ff8c96295b41193f162a5ce183096b1
Osward Phekani <oswarddesigns@gmail.com>
2025-05-27 23:48:02 +0100

    Array Plactice

--- projectTutorials/MyWebsiteFromScratch/practiceScripts.js
+++ projectTutorials/MyWebsiteFromScratch/practiceScripts.js
@@ -1,9 +1,9 @@
 const toDo = ["Code", "Gym", "Cook" ];
    toDo.push("GoToSleep ");
 
-for(let i = 0; i < toDo.length; i++){
-// console.log(toDo[i]);
-}
+// for(let i = 0; i < toDo.length; i++){
+// // console.log(toDo[i]);
+// }
  
 
 function halfNumbers(number){
@@ -12,3 +12,28 @@ const numberMultiply = number /2;
 console.log(numberMultiply);
 }
 halfNumbers(20);
+
+function multiplyByThree (number1){
+
+    const result = number1 * 3;
+    console.log(result);
+}
+multiplyByThree(5);
+
+const tomorrowTask = [
+    "1 Wake Up", 
+     "2 Meditate & Play ",
+      "3 Set Goals", 
+        "4 Wakeout", 
+          "5 Plan The day",
+            "6 Brush Teeth",
+                "7 eat BreakFast & French",
+                    "8 Go to Work "
+                ];
+
+                for(let i = 0 ; i < tomorrowTask.length; i++){
+                    console.log(tomorrowTask[i]);
+
+                }
+
+
