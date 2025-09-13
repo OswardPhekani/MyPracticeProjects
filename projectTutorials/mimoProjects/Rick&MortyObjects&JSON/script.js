d928b872dbefddc9ff34b43a0393f8feda94f2be
Osward Phekani <oswarddesigns@gmail.com>
2025-09-13 05:13:42 +0100

    Rick and Morty Data Insertion
    Create a new card and append data to HTML page

---
+++ projectTutorials/mimoProjects/Rick&MortyObjects&JSON/script.js
@@ -0,0 +1,62 @@
+//Create a JSON DATA STRUCTURE OF CHARACTERS
+const data = {
+"characters": [{
+      "name": "Rick Sanchez",
+      "status": "Alive",
+      "species": "Human",
+      "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+
+    },
+    {
+      "name": "Morty Smith",
+      "status": "Alive",
+      "species": "Human",
+      "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+    },
+    {
+      "name": "Summer Smith",
+      "status": "Alive",
+      "species": "Human",
+      "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+    }
+]
+};
+console.log(data);
+
+//refference the HTML Tag character-container in javascript 
+//use forEach function to access the data in the JSON FILE
+//Add a new DIV (card) to container 
+//add class to card 
+//appemd card to Character-container via characterContainer variable 
+
+// Add the data to card HTML FILE DYNAMICALLY 
+
+//reference HTML
+const characterContainer =document.getElementById("characters-container");
+
+//Access data
+data.characters.forEach(character => {
+// Create Card DIV
+const card = document.createElement("div");
+card.className = "card";
+characterContainer.appendChild(card);
+
+// Add Data to card HTML File 
+card.innerHTML = 
+`
+<img src="${character.image}" alt="${character.name}" class ="character-image">
+        <p class="character-details" >
+                <span class="attribute"> Name: </span>
+                     ${character.name}
+                </p> 
+            <p class="character-details" >
+                  <span class="attribute"> Status: </span>
+                     ${character.status}
+                    </p> 
+                 <p class="character-details" >
+                    <span class="attribute"> Species: </span>
+                      ${character.species}
+                     </p> 
+`;
+});
+
