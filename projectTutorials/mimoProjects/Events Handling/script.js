925a4c8ae9873206b859043b66e7d4b5369691c5
Osward Phekani <oswarddesigns@gmail.com>
2025-09-18 19:03:09 +0100

    events

---
+++ projectTutorials/mimoProjects/Events Handling/script.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('input');
+const log = document.getElementById('log');
+
+function updateValue() {
+ log.innerText = input.value;
+}
+input.
+onchange
+ = updateValue;
