721c369431b3075f00aa10a12cd7a8b674ff8967
Osward Phekani <oswarddesigns@gmail.com>
2025-09-19 05:20:16 +0100

    events handling

--- projectTutorials/mimoProjects/Events Handling/script.js
+++ projectTutorials/mimoProjects/Events Handling/script.js
@@ -7,3 +7,27 @@ function updateValue() {
 input.
 onchange
  = updateValue;
+
+//  Adding Multiple Events 
+const item = document.querySelector("div");
+const el = document.querySelectorAll("button");
+
+function changeWidth(){
+    item.classList.toggle("width");
+}
+function changeHeight(){
+    item.classList.toggle("height");
+}
+
+el[0].addEventListener("click", changeHeight);
+el[0].addEventListener("click", changeWidth);
+el[1].addEventListener("click", changeHeight);
+el[2].addEventListener("click", changeWidth);
+
+
+const itemDiv = document.querySelector("div");
+
+function changeColor() {
+ itemDiv.classList.toggle("pink");
+}
+itemDiv.addEventListener("click", changeColor);
