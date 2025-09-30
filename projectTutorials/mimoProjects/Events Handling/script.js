6c8e27da44d9a58f1d098becc9faa9b7b02f5c39
Osward Phekani <oswarddesigns@gmail.com>
2025-09-30 06:54:10 +0100

    Event Listeners
    Practicing Event Listeners, onscroll,onclick,ondblclick

--- projectTutorials/mimoProjects/Events Handling/script.js
+++ projectTutorials/mimoProjects/Events Handling/script.js
@@ -31,3 +31,52 @@ el[2].addEventListener("click", changeWidth);
 //  itemDiv.classList.toggle("pink");
 // }
 // itemDiv.addEventListener("click", changeColor);
+
+
+//EVENT LISTENERS LEVEL UP 
+
+// Change Text Area color on scroll 
+const textArea = document.querySelector("textarea");
+
+function  changeFontColor() {
+  textArea.style.color= "green";
+}
+textArea.onscroll = changeFontColor;
+
+
+// Change DIV tag Height 
+const div = document.querySelector("div");
+
+function changeHeight() {
+ div.classList.toggle("height");
+}
+div.onclick = changeHeight;
+
+// /* Change color DIV tag to blue when clicked using CSS */
+const itemBlue = document.querySelector("div");
+
+function changeColor() {
+ item.classList.toggle("blue");
+}
+item.ondblclick = changeColor;
+
+// <!-- Make Text Area directly  change Blue when Scrolled !-->
+const textArea = document.querySelector("textarea");
+
+function changeToBlue() {
+ textArea.style.backgroundColor = "dodgerBlue";
+}
+
+function changeToGreen() {
+ textArea.style.backgroundColor = "green";
+}
+textArea.onscroll = changeToBlue;
+
+// <!-- Make Text Area change Blue when Scrolled style is in CSS FIle !-->
+
+const itemRed = document.querySelector("div");
+
+function changeColor() {
+ itemRed.classList.toggle("red");
+}
+item.onclick = changeColor;
